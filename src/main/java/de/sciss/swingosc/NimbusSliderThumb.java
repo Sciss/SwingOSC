941c5f751946e1102e39be8445aec1a74a273817
Sciss <contact@sciss.de>
2012-02-17 23:05:09 +0000

    pressed thumb

--- src/main/java/de/sciss/swingosc/NimbusSliderThumb.java
+++ src/main/java/de/sciss/swingosc/NimbusSliderThumb.java
@@ -30,6 +30,14 @@ public class NimbusSliderThumb {
         };
     }
 
+
+    private static Color[] pressedGrad1colr( Color base ) {
+        return new Color[] {
+            NimbusHelper.adjustColor( base, -0.57865167f, -0.6357143f, -0.54509807f, 0 ),
+            NimbusHelper.adjustColor( base, 0.0015952587f, -0.04875779f, -0.18823531f, 0 )
+        };
+    }
+
     private static Color[] disabledGrad1colr( Color base ) {
         return new Color[] {
             NimbusHelper.adjustColor( base, 0.021348298f, -0.5625436f, 0.25490195f, 0 ),
@@ -43,6 +51,16 @@ public class NimbusSliderThumb {
             NimbusHelper.adjustColor( base, 0.021348298f, -0.5924243f, 0.35686272f, 0 ),
             NimbusHelper.adjustColor( base, 0.021348298f, -0.56722116f, 0.3098039f, 0 ),
             NimbusHelper.adjustColor( base, 0.021348298f, -0.56844974f, 0.32549018f, 0 )
+        };
+    }
+
+    private static final float[] pressedGrad2frac  = new float[] { 0.0f, 0.47593582f, 0.5962567f, 1.0f };
+    private static Color[] pressedGrad2colr( Color base ) {
+        return new Color[] {
+            NimbusHelper.adjustColor( base, 2.9569864e-4f, -0.44943976f, 0.25098038f, 0 ),
+            NimbusHelper.adjustColor( base, 0.0f, 0.0f, 0.0f, 0 ),
+            NimbusHelper.adjustColor( base, 0.0f, 0.0f, 0.0f, 0 ),
+            NimbusHelper.adjustColor( base, 8.9377165e-4f, -0.121094406f, 0.12156862f, 0 )
         };
     }
 
@@ -82,13 +100,63 @@ public class NimbusSliderThumb {
                 MultipleGradientPaint.CycleMethod.NO_CYCLE );
     }
 
+    private Paint createPressedGradient1( Color base, float shpX1, float shpY1, float shpW, float shpH ) {
+        final float startX = shpX1 + 0.510610079575597f * shpW;
+        final float startY = shpY1 + -4.553649124439119e-18f * shpH;
+        final float endX   = shpX1 + 0.4993368700265251f * shpW;
+        final float endY   = shpY1 + 1.0039787798408488f * shpH;
+        return new LinearGradientPaint( startX, startY, endX, endY, grad1frac, pressedGrad1colr( base ),
+                MultipleGradientPaint.CycleMethod.NO_CYCLE );
+    }
+
+    private Paint createPressedGradient2( Color base, float shpX1, float shpY1, float shpW, float shpH ) {
+        final float startX = shpX1 + 0.5023510971786834f * shpW;
+        final float startY = shpY1 + 0.0015673981191222587f * shpH;
+        final float endX   = shpX1 + 0.5023510971786838f * shpW;
+        final float endY   = shpY1 + 1.0f * shpH;
+        return new LinearGradientPaint( startX, startY, endX, endY, pressedGrad2frac, pressedGrad2colr( base ),
+                MultipleGradientPaint.CycleMethod.NO_CYCLE );
+    }
+
     public void paint( int state, Color c, Graphics2D g, int x, int y, int width, int height ) {
         final Color base = NimbusHelper.mixColorWithAlpha( NimbusHelper.getBaseColor(), c );
         if( (state & NimbusHelper.STATE_ENABLED) != 0 ) {
-            paintEnabled( g, base, x, y, width, height );
+            if( (state & NimbusHelper.STATE_PRESSED) != 0 ) {
+                paintPressed( g, base, x, y, width, height );
+            } else {
+                paintEnabled( g, base, x, y, width, height );
+            }
         } else {
             paintDisabled( g, base, x, y, width, height );
         }
+    }
+
+    private void paintPressed( Graphics2D g, Color base, int x, int y, int width, int height ) {
+        final float e1x = x + 2f;
+        final float e1y = y + 3f;
+        final float e1w = width - 4f;
+        final float e1h = height - 4f;
+        ellip.setFrame( e1x, e1y, e1w, e1h );
+        final Color c1 = NimbusHelper.adjustColor( NimbusHelper.getBlueGreyColor( base ),
+                0f, -0.110526316f, 0.25490195f, -121 );
+        g.setColor( c1 );
+        g.fill( ellip );
+
+        final float e2x = x + 2f;
+        final float e2y = y + 2f;
+        final float e2w = width - 4f;
+        final float e2h = height - 4f;
+        ellip.setFrame( e2x, e2y, e2w, e2h );
+        g.setPaint( createPressedGradient1( base, e2x, e2y, e2w, e2h ));
+        g.fill( ellip );
+
+        final float e3x = x + 3f;
+        final float e3y = y + 3f;
+        final float e3w = width - 6f;
+        final float e3h = height - 6f;
+        ellip.setFrame( e3x, e3y, e3w, e3h );
+        g.setPaint( createPressedGradient2( base, e3x, e3y, e3w, e3h ));
+        g.fill( ellip );
     }
 
     private void paintDisabled( Graphics2D g, Color base, int x, int y, int width, int height ) {
@@ -110,8 +178,6 @@ public class NimbusSliderThumb {
     }
 
     private void paintEnabled( Graphics2D g, Color base, int x, int y, int width, int height ) {
-        // <size width="17" height="17"/>
-
         final float e1x = x + 2f;
         final float e1y = y + 3f;
         final float e1w = width - 4f;
@@ -137,12 +203,5 @@ public class NimbusSliderThumb {
         ellip.setFrame( e3x, e3y, e3w, e3h );
         g.setPaint( createEnabledGradient2( base, e3x, e3y, e3w, e3h ));
         g.fill( ellip );
-
-//        <ellipse x1="2.0" x2="15.0" y1="2.0" y2="15.0">
-//        </ellipse>
-//        <ellipse x1="2.0" x2="15.0" y1="3.0" y2="16.0">
-//           <matte red="111" green="116" blue="125" alpha="99" uiDefaultParentName="nimbusBlueGrey" hueOffset="-0.003968239" saturationOffset="0.0014736876" brightnessOffset="-0.25490198" alphaOffset="-156"/>
-//           <paintPoints x1="0.25" y1="0.0" x2="0.75" y2="1.0"/>
-//        </ellipse>
     }
 }
